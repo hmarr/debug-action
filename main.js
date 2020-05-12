5badc37e5a3bfad0997f85f17e3ea6e20390b04f
Jessica Petty <jepetty@microsoft.com>
2020-05-12 14:44:46 -0700

    don't print GitHub directly as it should just be in the process.env variables

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-'no-strict';
 
-const github = require('@actions/github');
-
-Object.defineProperty(github, 'github', {enumerable: true});
 Object.defineProperty(process.env, 'env', { enumerable: true});
-console.log('-------- GitHub Properties --------')
-for (const prop in github) {
-    console.log(`${prop}: ${github[prop]}`);
-}
 
 console.log('\n-------- Process.ENV properties --------')
 for (const prop in process.env) {
