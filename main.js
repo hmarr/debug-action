b3a05a3b068af9cff0030b983f38479021647d37
Jessica Petty <jepetty@microsoft.com>
2020-05-12 14:56:57 -0700

    fix code

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+import * as github from '@actions/github';
 
+Object.defineProperty(github.context, 'github', {enumerable: true});
 Object.defineProperty(process.env, 'env', { enumerable: true});
+console.log('-------- GitHub Properties --------')
+for (const prop in github.context) {
+    console.log(`${prop}: ${github.context[prop]}`);
+}
+Object.defineProperty(github.context.payload, 'payload', {enumerable: true});
+for (const prop in github.context.payload) {
+    console.log(`${prop}: ${github.context.payload[prop]}`)
+}
 
 console.log('\n-------- Process.ENV properties --------')
 for (const prop in process.env) {
